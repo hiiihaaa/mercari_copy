0afc4c3e0d625470b043d4b741a7eb7f20c52411
hiiihaaa <snshiiihaaa@gmail.com>
2020-03-08 00:39:23 +0900

    user_registration1

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  def new
+    # super
+    @user = User.new
+  end
+
+  # POST /resource
+  def create
+    # super
+    para_u = params[:user]
+    @user = User.new(user_params)
+    @user.birthday = Date.new(para_u["birthday(1i)"].to_i, para_u["birthday(2i)"].to_i, para_u["birthday(3i)"].to_i).strftime("%Y-%m-%d")
+    @user.save
+    redirect_to root
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:nickname, :lastname, :firstname, :lastname_kana, :firstname_kana, :email, :birthday, :password)
+    end
+
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+end
---
+++ db/migrate/20200306154318_devise_create_users.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :firstname,          null: false
+      t.string :lastname,           null: false
+      t.string :firstname_kana,     null: false
+      t.string :lastname_kana,      null: false
+      t.date   :birthday,           null: false
+      t.string :user_img
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+      # t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+
+  end
+end
